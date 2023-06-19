74576e5712093db795a3414c6e9b8f73a126b96c
nascimentodorafael <107429969+nascimentodorafael@users.noreply.github.com>
2023-06-19 15:09:14 -0300

    Add files via upload

---
+++ imc_now/js/main.js
@@ -0,0 +1,7 @@
+// Capturar os dados do formulário
+const pega = document.querySelector('#formulario');
+
+pega.addEventListener('submit', function(event) {
+    event.preventDefault();
+    console.log('teste');
+});
