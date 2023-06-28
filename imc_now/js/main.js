52f85a70d670ea845b833b3878c1e27e76f05310
nascimentodorafael <107429969+nascimentodorafael@users.noreply.github.com>
2023-06-28 14:31:57 -0300

    Delete imc_now directory

--- imc_now/js/main.js
+++
@@ -1,7 +0,0 @@
-// Capturar os dados do formulário
-const pega = document.querySelector('#formulario');
-
-pega.addEventListener('submit', function(event) {
-    event.preventDefault();
-    console.log('teste');
-});
